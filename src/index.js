3ce59b5f626a9077cdbc8236153281c0f45530ea
i62navpm <manuelnavarro1987@gmail.com>
2017-12-22 20:49:50 +0100

    New: React app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-console.log('hello world')
-console.log('hola')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
